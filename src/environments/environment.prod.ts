d18e80896f29bcfcbec45dd3636580340ab4a1e0
Siyana Atanasova <s.e.atanasowa@gmail.com>
2023-03-30 13:47:39 +0300

    Revert "testing github pages"
    This reverts commit a5b37de90687f94390a35950646dcc9549287af4.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoic2l5YW5hLWF0YW5hc292YS1kZXYiLCJhIjoiY2xkMDk0cjhzMDBmZzNwcDlmaWkxN2gzbSJ9.wBLviMWYxvlDaEhvxd48tg'
+  }
 };
