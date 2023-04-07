f38429074a6ca4958d4f0bc7ec9013d5d29c7735
Siyana Atanasova <s.e.atanasowa@gmail.com>
2023-04-07 16:57:56 +0300

    changed apiURL to IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   mapbox: {
     accessToken: 'pk.eyJ1Ijoic2l5YW5hLWF0YW5hc292YS1kZXYiLCJhIjoiY2xkMDk0cjhzMDBmZzNwcDlmaWkxN2gzbSJ9.wBLviMWYxvlDaEhvxd48tg'
   },
-  apiURL: 'http://localhost:3000'
+  // apiURL: 'http://localhost:3000'
+  apiURL: 'http://77.71.12.146:3000'
 };
 
 /*
