3915e0e9936dde54a45006533ff2c74fcc4102ff
Siyana Atanasova <s.e.atanasowa@gmail.com>
2023-04-11 13:19:33 +0300

    changed api URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
     accessToken: 'pk.eyJ1Ijoic2l5YW5hLWF0YW5hc292YS1kZXYiLCJhIjoiY2xkMDk0cjhzMDBmZzNwcDlmaWkxN2gzbSJ9.wBLviMWYxvlDaEhvxd48tg'
   },
   // apiURL: 'http://localhost:3000'
-  apiURL: 'https://77.71.12.146:3000'
+  // apiURL: 'https://77.71.12.146:3000'
+  apiURL: 'https://ninety-corners-cover-77-71-12-146.loca.lt/'
 };
 
 /*
