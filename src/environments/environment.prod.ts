16404675373a0d63f1b645319c3a8934db437fd1
Siyana Atanasova <s.e.atanasowa@gmail.com>
2023-04-11 13:35:18 +0300

    changed prod API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     accessToken: 'pk.eyJ1Ijoic2l5YW5hLWF0YW5hc292YS1kZXYiLCJhIjoiY2xkMDk0cjhzMDBmZzNwcDlmaWkxN2gzbSJ9.wBLviMWYxvlDaEhvxd48tg'
   },
   // apiURL: 'http://localhost:3000'
-  apiURL: 'https://77.71.12.146:3000'
+  apiURL: 'https://delivery-app-dev.loca.lt'
 };
